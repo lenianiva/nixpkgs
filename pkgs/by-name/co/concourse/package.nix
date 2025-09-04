f94733e61e751f86194f81a31bce2b1903095165
LightQuantum <self@lightquantum.me>
2025-09-03 23:30:45 -0400

    feat: build concourse from source

--- pkgs/by-name/co/concourse/package.nix
+++ pkgs/by-name/co/concourse/package.nix
@@ -1,38 +1,107 @@
 {
-  stdenv,
+  nodejs,
+  elmPackages,
+  yarn-berry_4,
+  buildGo125Module,
+  fetchFromGitHub,
   lib,
-  autoPatchelfHook,
+  stdenv,
+  writeScript,
 }:
 
-stdenv.mkDerivation rec {
-  pname = "concourse";
+let
   version = "7.14.1";
 
-  src = fetchTarball {
-    url = "https://github.com/concourse/concourse/releases/download/v${version}/concourse-${version}-linux-amd64.tgz";
-    sha256 = "5228ecd88c491c814db023c72bf9c5c38b9a0b99d2dba90d84eba34e55e2bac9";
+  yarn-berry = yarn-berry_4;
+  buildGoModule = buildGo125Module;
+in
+buildGoModule rec {
+  inherit version;
+  pname = "concourse";
+  src = fetchFromGitHub {
+    owner = "concourse";
+    repo = "concourse";
+    rev = version;
+    hash = "sha256-Q+j41QhhibyE+a7iOgMKm2SeXhNV8ek97P014Wje9NQ=";
   };
 
-  nativeBuildInputs = [
-    autoPatchelfHook
+  webui = stdenv.mkDerivation (finalAttrs: {
+    inherit pname version src;
+
+    nativeBuildInputs = [
+      nodejs
+      yarn-berry
+      yarn-berry.yarnBerryConfigHook
+    ];
+
+    env = {
+      PUPPETEER_SKIP_DOWNLOAD = "true";
+    };
+
+    missingHashes = ./missing-hashes.json;
+    offlineCache = yarn-berry.fetchYarnBerryDeps {
+      inherit (finalAttrs) src missingHashes;
+      hash = "sha256-vVfLdJSXfYlWiLXKJ0ywSnBMhpJbFrikfccbX/XJlzU=";
+    };
+
+    postConfigure = elmPackages.fetchElmDeps {
+      elmPackages = import ./elm-srcs.nix;
+      elmVersion = elmPackages.elm.version;
+      registryDat = ./registry.dat;
+    };
+
+    preBuild =
+      let
+        # NOTE the node wrapper is required because yarn 2+ always executes elm as a node script
+        elmWrapperScript = writeScript "elm-wrapper" ''
+          #!/usr/bin/env node
+
+          var child_process = require('child_process');
+          child_process.spawn('${lib.getExe elmPackages.elm}', process.argv.slice(2), { stdio: 'inherit' })
+            .on('exit', process.exit);
+        '';
+      in
+      ''
+        cp ${elmWrapperScript} node_modules/elm/bin/elm
+      '';
+
+    buildPhase = ''
+      runHook preBuild
+      yarn run build
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp -r web/public $out
+      runHook postInstall
+    '';
+  });
+
+  vendorHash = "sha256-2busKAFaQYE82XKCAx8BGOMjjs8WzqIxdpz+J45maoc=";
+  subPackages = [ "cmd/concourse" ];
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/concourse/concourse.Version=${version}"
   ];
 
-  buildInputs = [
-  ];
-
-  sourceRoot = ".";
-
-  installPhase = ''
-    runHook preInstall
-    cp -r $src/bin $out/bin
-    cp -r $src/fly-assets $out/fly-assets
-    cp -r $src/resource-types $out/resource-types
-    runHook postInstall
+  preBuild = ''
+    cp -r ${webui}/public web
   '';
 
+  doCheck = false; # TODO tests broken
+
+  # TODO install check
+
+  # TODO update script
+
   meta = with lib; {
-    homepage = "https://concourse-ci.org/";
-    description = "Concourse is an open-source continuous thing-doer.";
+    homepage = "https://concourse-ci.org";
+    description = "A container-based automation system written in Go.";
     platforms = lib.platforms.linux ++ lib.platforms.darwin;
+    license = licenses.asl20;
+    # TODO maintainers
   };
 }
